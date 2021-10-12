001abe2a70ad05b933488ba112f00d5e8a0b42be
Christina Whethers <flandresamarawr@gmail.com>
2021-10-12 13:38:19 -0400

    Added preliminary error handling

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { handleErrors, handleValidationErrors } = require('./middleware/custom_errors')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).send(message)
+})
 
+app.get('/', (req, res) => {
+    res.send("Hello, world!")
+})
+
+app.use(handleValidationErrors)
+app.use(handleErrors)
 app.set("port", process.env.PORT || 8080)
 app.listen(app.get("port"), () => {
     console.log("Listening on port ", app.get("port"));
